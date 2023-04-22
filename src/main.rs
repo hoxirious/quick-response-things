57aa3853efd5c6c920d6e4c45485d41dd68b22e6
Hy Huynh <giahy33@gmail.com>
2023-04-22 13:34:48 -0600

    quyen-beo commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use qrcode::QrCode;
+use image::Luma;
+
 fn main() {
-    println!("Hello, world!");
+    let code = QrCode::new(&b"Quyen Beo"[..]).unwrap();
+
+// Render the bits into an image.
+    let image = code.render::<Luma<u8>>().build();
+
+// Save the image.
+image.save("/home/hoxirious/qrcode.png").unwrap();
+
+// You can also render it into a string.
+let string = code.render()
+    .light_color(' ')
+    .dark_color('#')
+    .build();
+println!("{}", string);
 }
