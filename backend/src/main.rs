e02e90e13c59b1ec00a311457cdbe0a6f75aa46b
Hy Huynh <giahy33@gmail.com>
2023-04-23 19:26:17 -0600

    rewrite lib successfully

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,34 @@
-use image::Luma;
+use image::{Luma, ColorType, ImageResult, EncodableLayout, ImageEncoder, codecs::png};
 use qrcode::{QrCode};
 use actix_web::{ post, web, App, HttpResponse, HttpServer};
 use actix_cors::Cors;
 use std::io::{Cursor, Write, Seek, SeekFrom, Read};
 use serde::Deserialize;
+
+
+pub fn write_buffer_impl<W: std::io::Write + Seek>(
+    buffered_write: &mut W,
+    buf: &[u8],
+    width: u32,
+    height: u32,
+    color: ColorType,
+) -> ImageResult<()> {
+    png::PngEncoder::new(buffered_write).write_image(buf, width, height, color)
+}
+
+pub fn write_buffer_with_format<W>(
+    buffered_writer: &mut W,
+    buf: &[u8],
+    width: u32,
+    height: u32,
+    color: ColorType,
+) -> ImageResult<()>
+where
+    W: Write + Seek,
+{
+    // thin wrapper function to strip generics
+    write_buffer_impl(buffered_writer, buf, width, height, color)
+}
 
 #[derive(Deserialize)]
 struct Wifi {
@@ -31,15 +56,19 @@ async fn encode_wifi(wifi: web::Form<Wifi>) -> HttpResponse {
     let wifi_auth =Wifi {
         name: wifi.name.clone(),
         connection_type: wifi.connection_type.clone(),
-        password: wifi.password.clone(),        
+        password: wifi.password.clone(),
     };
 
     let encoded_wifi = wifi_auth.encode_wifi();
 
     let code = QrCode::new::<String>(encoded_wifi).unwrap();
 
-    // Render the bits into an image.   
+    // Render the bits into an image.
     let image = code.render::<Luma<u8>>().build();
+
+    let mut bytes = Vec::new();
+
+    write_buffer_with_format(&mut Cursor::new(&mut bytes), image.as_bytes(), image.width(), image.height(), ColorType::L8).expect("Cannot handle this data");
 
     let mut writer = Cursor::new(Vec::new());
 
@@ -50,16 +79,16 @@ async fn encode_wifi(wifi: web::Form<Wifi>) -> HttpResponse {
     writer.read_to_end(&mut out).unwrap();
     //writer.write_to(&mut buf, image::ImageOutputFormat::Png)?;
     // Save the image to path.
-    image.save("/home/hyhuynh/qrcode.png").unwrap();
+    image.save("/tmp/qrcode.png").unwrap();
 
     // Load image from path.
-    let image_content = web::block(|| std::fs::read("/home/hyhuynh/qrcode.png")).await.unwrap().unwrap();
+    let image_content = web::block(|| std::fs::read("/tmp/qrcode.png")).await.unwrap().unwrap();
     println!("{:?}", image_content);
     println!("Ok");
     
     HttpResponse::Ok()
     .content_type("image/png")
-    .body(out)
+    .body(bytes)
 
 }
 
